883387c4c1c26b39f2e6076687e01540fe077f62
Ali Fener <alifener@gmail.com>
2018-02-26 23:21:53 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+   region = "eu-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami            =  "ami-403e2524"
+  instance_type  =  "t2.micro"
+
+  tags {
+    Name = "first terraform instance"
+  }
+
+}
